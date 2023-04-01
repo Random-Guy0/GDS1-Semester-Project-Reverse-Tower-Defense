0691c2d55f5a4a8aa42bdc6c901e614123fdcf2f
CoveringSky1 <barryli12138@gmail.com>
2023-04-02 01:14:11 +1100

    health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 100f;
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DamageTake(float damage)
+    {
+        health -= damage;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Attack"))
+        {
+            //DamagerTake(damage)
+        }
+    }
+}
