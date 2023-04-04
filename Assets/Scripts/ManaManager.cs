39f6c4b1ba27538ab8f9097b3a3150c6e0c02aea
KevinPhu1999 <1999kevinphu@gmail.com>
2023-04-04 12:24:00 +1000

    Added mana counter script

--- Assets/Scripts/ManaManager.cs
+++ Assets/Scripts/ManaManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ManaManager : MonoBehaviour
 {
+    public static ManaManager instance;
+    public TMP_Text currentManaCount;
+
     public int currentMana;
     public int maxMana;
     public int minMana;
@@ -12,6 +17,11 @@ public class ManaManager : MonoBehaviour
     {
         maxMana = 100;
         minMana = 0;
+        instance = this;
+    }
+    void Start()
+    {
+        currentManaCount.text = "Mana: " + currentMana.ToString();
     }
 
     public int getMana() 
@@ -24,9 +34,11 @@ public class ManaManager : MonoBehaviour
         if (currentMana + mana >= maxMana)
         {
             currentMana = maxMana;
+            currentManaCount.text = "Mana: " + currentMana.ToString();
             return;
         }
         currentMana = currentMana + mana;
+        currentManaCount.text = "Mana: " + currentMana.ToString();
     }
 
     public void costMana(int cost)
