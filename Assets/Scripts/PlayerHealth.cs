7b23f5e1b6bc80faf240e820adc60e14090987e4
CoveringSky1 <barryli12138@gmail.com>
2023-04-05 23:29:26 +1000

    Update PlayerHealth.cs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -19,7 +19,7 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (health >= 0)
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
