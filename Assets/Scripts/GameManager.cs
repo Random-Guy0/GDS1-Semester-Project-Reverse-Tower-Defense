f44f5a63d3dcba5fda64740f82ba28362b859a16
Amren Gill <amren.gill@gmail.com>
2023-04-17 21:09:23 +1000

    level health counter now works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private string levelHealthTextPrefix;
+    [SerializeField] private TMP_Text levelHealthText;
     [SerializeField] private GameObject winText;
     [SerializeField] private int levelHealth;
+
+    private void Start()
+    {
+        SetHealthText();
+    }
 
     public void TakeDamage()
     {
         levelHealth--;
+        SetHealthText();
         if (levelHealth <= 0)
         {
             winText.SetActive(true);
@@ -21,5 +30,10 @@ public class GameManager : MonoBehaviour
     private void Close()
     {
         Application.Quit();
+    }
+
+    private void SetHealthText()
+    {
+        levelHealthText.SetText(levelHealthTextPrefix + ": " + levelHealth);
     }
 }
