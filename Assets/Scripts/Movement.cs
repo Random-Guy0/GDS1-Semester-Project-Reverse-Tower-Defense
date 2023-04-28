89c35b594637ecc734c5b175d0138945f66fa266
CoveringSky1 <barryli12138@gmail.com>
2023-04-28 22:19:08 +1000

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,7 @@ public class Movement : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed =10.0f;
+    private float playerSpeed =5.0f;
     private float gravityValue = -9.81f;
     public GameObject Camera;
     public string cameraS;
@@ -22,55 +22,59 @@ public class Movement : MonoBehaviour
         cameraS = Camera.GetComponent<camera>().getCurrentCamera();
     }
 
-    void Update()
+    private void Move()
+    {
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        playerVelocity.y += gravityValue * Time.deltaTime;
+    }
+
+    private void UpdateAnimator()
+    {
+        if (move != Vector3.zero)
+        {
+            animator.SetBool("IsMove", true);
+            gameObject.transform.forward = move;
+        }
+        else
+        {
+            animator.SetBool("IsMove", false);
+        }
+
+        if (health <= 0)
+        {
+            animator.SetTrigger("Death");
+        }
+    }
+
+    private void Update()
     {
         health = GetComponent<PlayerHealth>().health;
-        cameraS = Camera.GetComponent<camera>().getCurrentCamera();
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
-        if(health <= 0)
-        {
-            animator.SetTrigger("Death");
-        }
-
-
+        cameraS = Camera.GetComponent<camera>().getCurrentCamera();
 
         switch (cameraS)
         {
             case "AngleOne":
-                move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-                controller.Move(move * Time.deltaTime * playerSpeed);
+                move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
                 break;
             case "AngleTwo":
-                move = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal")).normalized;
-                controller.Move(move * Time.deltaTime * playerSpeed);
+                move = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
                 break;
             case "AngleThree":
-                move = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical")).normalized;
-                controller.Move(move * Time.deltaTime * playerSpeed);
+                move = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
                 break;
             case "AngleFour":
-                move = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal")).normalized;
-                controller.Move(move * Time.deltaTime * playerSpeed);
+                move = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
                 break;
         }
 
-        if (move != Vector3.zero)
-        {
-            animator.SetBool("IsMove", true);
-            gameObject.transform.forward = move;
-        }
-        else if(move == Vector3.zero)
-        {
-            animator.SetBool("IsMove", false);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        Move();
+        UpdateAnimator();
     }
 
     private void OnTriggerEnter(Collider other)
